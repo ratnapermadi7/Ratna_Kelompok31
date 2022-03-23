a5b296ce4b1fff31b11830a9107bf95c15e23fa9
ratnapermadi7 <96427156+ratnapermadi7@users.noreply.github.com>
2022-03-23 15:43:42 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("hello ini tugas modul 4 ya");
+        System.out.println("kerjain di intelej baru copy sini gan");
     }
 }
